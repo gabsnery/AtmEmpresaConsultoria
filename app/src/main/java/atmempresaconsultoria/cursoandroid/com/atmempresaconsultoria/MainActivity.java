29041b690031ded68c5aafbb0cf3a295360a156c
gabsnery <gneri94@gmail.com>
2018-04-27 23:46:13 -0300

    Acerto de ContraintLayout

--- app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -5,12 +5,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    ImageView botaoServico;
+    ImageView botaoCliente;
+    ImageView botaoContato;
+    ImageView botaoEmpresa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoServico = findViewById(R.id.servicoId);
+        botaoCliente = findViewById(R.id.clienteId);
+        botaoContato = findViewById(R.id.contatoId);
+        botaoEmpresa = findViewById(R.id.empresaId);
+
+        botaoServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ServicoActivity.class));
+            }
+        });
+        botaoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ClienteActivity.class));
+            }
+        });
+        botaoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ContatoActivity.class));
+            }
+        });
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ActivityEmpresa.class));
+            }
+        });
     }
 }

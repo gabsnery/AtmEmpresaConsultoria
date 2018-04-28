bd5f243de49d43a809b14c6aee4e5514ea6828f8
gabsnery <gneri94@gmail.com>
2018-04-27 21:20:08 -0300

    Tela principal com os botões chamando as outras telas.

--- app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -5,13 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
-    private ImageView botaoEmpresa;
-    private ImageView botaoCliente;
-    private ImageView botaoContato;
-    private ImageView botaoServico;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
